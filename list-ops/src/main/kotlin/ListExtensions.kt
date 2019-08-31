fefd95bcad4589d6a27a43cff7d011ab94244e82
Yohei Omura <yomura7@gmail.com>
2019-09-01 01:06:43 +0900

    solve list-ops

--- list-ops/src/main/kotlin/ListExtensions.kt
+++ list-ops/src/main/kotlin/ListExtensions.kt
@@ -1,34 +1,66 @@
 package extensions
 
-fun <T> List<T>.customAppend(list: List<T>): List<T> {
-  return emptyList()
+fun <T> List<T>.customAppend(that: List<T>): List<T> {
+    val list = this.toMutableList()
+    that.forEach{ list.add(it) }
+    return list
 }
 
 fun <T> List<List<T>>.customConcat(): List<T> {
-  return emptyList()
-}
-
-fun <T> List<T>.customReverse(): List<T> {
-  return emptyList()
-}
-
-fun <T> List<T>.customMap(predicate: (T) -> T): List<T> {
-  val x = 16.0.times(3)
-  return emptyList()
+    val list = mutableListOf<T>()
+    this.forEach{ subList ->
+        subList.forEach {
+            list.add(it)
+        }
+    }
+    return list
 }
 
 fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> {
-  return emptyList()
+    val list = mutableListOf<T>()
+    this.forEach {
+        if (predicate(it)) list.add(it)
+    }
+    return list
 }
 
-fun <T, V> List<T>.customFoldRight(item: V , accumulator: (V, T) -> V ): V {
-  return 0 as V
+val <T> List<T>.customSize: Int
+    get() {
+        var size = 0
+        this.forEach { size++ }
+        return size
+    }
+
+fun <T> List<T>.customMap(predicate: (T) -> T): List<T> {
+    val list = mutableListOf<T>()
+    this.forEach {
+        list.add(predicate(it))
+    }
+    return list
 }
 
 fun <T, V> List<T>.customFoldLeft(item: V , accumulator: (V, T) -> V ): V {
-  return 0 as V
+    var list = item
+    this.forEach {
+        list = accumulator(list, it)
+    }
+    return list
+}
+
+fun <T, V> List<T>.customFoldRight(item: V , accumulator: (T, V) -> V ): V {
+    var list = item
+    this.customReverse().forEach {
+        list = accumulator(it, list)
+    }
+    return list
+}
+
+fun <T> List<T>.customReverse(): List<T> {
+    var list = mutableListOf<T>()
+    this.dropLastWhile {
+        list.add(it)
+    }
+    return list
 }
 
 
-val <T> List<T>.customSize: Int
-  get() = 0
--- list-ops/src/test/kotlin/ListExtensionsTest.kt
+++ list-ops/src/test/kotlin/ListExtensionsTest.kt
@@ -12,7 +12,6 @@ class ListExtensionsTest {
                 emptyList<Int>().customAppend(emptyList()))
     }
 
-    @Ignore
     @Test
     fun testAppendingNonEmptyListOnEmptyList() {
         assertEquals(
@@ -20,7 +19,6 @@ class ListExtensionsTest {
                 emptyList<Char>().customAppend(listOf('1', '2', '3', '4')))
     }
 
-    @Ignore
     @Test
     fun testAppendingNonEmptyListOnNonEmptyList() {
         assertEquals(
@@ -28,7 +26,6 @@ class ListExtensionsTest {
                 listOf("1", "2").customAppend(listOf("2", "3", "4", "5")))
     }
 
-    @Ignore
     @Test
     fun testConcatOnEmptyListOfLists() {
         assertEquals(
@@ -36,7 +33,6 @@ class ListExtensionsTest {
                 emptyList<List<Int>>().customConcat())
     }
 
-    @Ignore
     @Test
     fun testConcatOnNonEmptyListOfLists() {
         assertEquals(
@@ -44,7 +40,6 @@ class ListExtensionsTest {
                 listOf(listOf('1', '2'), listOf('3'), emptyList(), listOf('4', '5', '6')).customConcat())
     }
 
-    @Ignore
     @Test
     fun testFilteringEmptyList() {
         assertEquals(
@@ -52,7 +47,6 @@ class ListExtensionsTest {
                 emptyList<Int>().customFilter { it % 2 == 1 })
     }
 
-    @Ignore
     @Test
     fun testFilteringNonEmptyList() {
         assertEquals(
@@ -60,19 +54,16 @@ class ListExtensionsTest {
                 listOf(1, 2, 3, 5).customFilter { it % 2 == 1 })
     }
 
-    @Ignore
     @Test
     fun testSizeOfEmptyList() {
         assertEquals(0, emptyList<Int>().customSize)
     }
 
-    @Ignore
     @Test
     fun testSizeOfNonEmptyList() {
         assertEquals(4, listOf("one", "two", "three", "four").customSize)
     }
 
-    @Ignore
     @Test
     fun testTransformingEmptyList() {
         assertEquals(
@@ -80,7 +71,6 @@ class ListExtensionsTest {
                 emptyList<Int>().customMap { it -> it + 1 })
     }
 
-    @Ignore
     @Test
     fun testTransformingNonEmptyList() {
         assertEquals(
@@ -88,7 +78,6 @@ class ListExtensionsTest {
                 listOf(1, 3, 5, 7).customMap { it -> it + 1 })
     }
 
-    @Ignore
     @Test
     fun testFoldLeftOnEmptyList() {
         assertEquals(
@@ -96,7 +85,6 @@ class ListExtensionsTest {
                 emptyList<Int>().customFoldLeft(2.0, Double::times))
     }
 
-    @Ignore
     @Test
     fun testFoldLeftWithDirectionIndependentOperationOnNonEmptyList() {
         assertEquals(
@@ -104,7 +92,6 @@ class ListExtensionsTest {
                 listOf(1, 2, 3, 4).customFoldLeft(5, Int::plus))
     }
 
-    @Ignore
     @Test
     fun testFoldLeftWithDirectionDependentOperationOnNonEmptyList() {
         assertEquals(
@@ -112,7 +99,6 @@ class ListExtensionsTest {
                 listOf(2, 5).customFoldLeft(5, Int::div))
     }
 
-    @Ignore
     @Test
     fun testFoldRightOnEmptyList() {
         assertEquals(
@@ -120,7 +106,6 @@ class ListExtensionsTest {
                 emptyList<Double>().customFoldRight(2.0, Double::times))
     }
 
-    @Ignore
     @Test
     fun testFoldRightWithDirectionIndependentOperationOnNonEmptyList() {
         assertEquals(
@@ -128,7 +113,6 @@ class ListExtensionsTest {
                 listOf(1, 2, 3, 4).customFoldRight(5, Int::plus))
     }
 
-    @Ignore
     @Test
     fun testFoldRightWithDirectionDependentOperationOnNonEmptyList() {
         assertEquals(
@@ -136,7 +120,6 @@ class ListExtensionsTest {
                 listOf(2, 5).customFoldRight(5, Int::div))
     }
 
-    @Ignore
     @Test
     fun testReversingEmptyList() {
         assertEquals(
@@ -144,7 +127,6 @@ class ListExtensionsTest {
                 emptyList<Int>().customReverse())
     }
 
-    @Ignore
     @Test
     fun testReversingNonEmptyList() {
         assertEquals(
