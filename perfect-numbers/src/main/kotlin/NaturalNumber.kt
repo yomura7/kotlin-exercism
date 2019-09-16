a249342ea9bdba574e45ffc71571f4bdbaf18c78
Yohei Omura <yomura7@gmail.com>
2019-09-16 14:10:32 +0900

    solve perfect-numbers

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -4,5 +4,17 @@ enum class Classification {
 }
 
 fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+    if (naturalNumber <= 0) throw RuntimeException("Input must be a natural number")
+    var count = 0
+    for (n in 1 until naturalNumber) {
+        if ( naturalNumber % n == 0) {
+            count += n
+        }
+    }
+    return when {
+        count > naturalNumber -> Classification.ABUNDANT
+        count == naturalNumber -> Classification.PERFECT
+        count < naturalNumber -> Classification.DEFICIENT
+        else -> throw RuntimeException("Unknown case")
+    }
 }
