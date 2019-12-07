ab506eb44d8db44c139dc003bdd085f6785007ef
Yohei Omura <yomura7@gmail.com>
2019-12-07 17:54:40 +0900

    solve binary-search

--- binary-search/src/main/kotlin/BinarySearch.kt
+++ binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,14 @@
 object BinarySearch {
     fun search(list: List<Int>, item: Int): Int {
-        TODO("Implement the function to complete the task. Change the signature if necessary.")
+        require(list.isNotEmpty()) {
+            throw NoSuchElementException()
+        }
+        val middle = list.size / 2
+        return when {
+            list[middle] == item -> middle
+            list[middle] > item -> search(list.subList(0, middle), item)
+            list[middle] < item -> search(list.subList(middle + 1, list.size), item) + (middle + 1)
+            else -> throw NoSuchElementException()
+        }
     }
 }
