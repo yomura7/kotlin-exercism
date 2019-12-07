e63dcb435f67141980a3e07b3388024a123f818e
Yohei Omura <yomura7@gmail.com>
2019-12-07 13:41:44 +0900

    solve sieve

--- sieve/src/main/kotlin/Sieve.kt
+++ sieve/src/main/kotlin/Sieve.kt
@@ -1,6 +1,15 @@
 object Sieve {
-
     fun primesUpTo(upperBound: Int): List<Int> {
-        TODO("Implement this function to complete the task")
+        val intList = (2..upperBound).toList()
+        val markedIntSet = mutableSetOf<Int>()
+        intList.forEach {p ->
+            if (markedIntSet.contains(p)) return@forEach
+            var i = 0
+            while(2 * p + i * p <= upperBound) {
+                markedIntSet.add(2 * p + i * p)
+                i++
+            }
+        }
+        return intList - markedIntSet
     }
 }
